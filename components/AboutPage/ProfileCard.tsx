88483f4c189846e585448fbf060f88bd7f6301fe
Michael Gummere <me@disilla.org>
2024-02-05 06:58:49 -0500

    improve profile card

--- components/AboutPage/ProfileCard.tsx
+++ components/AboutPage/ProfileCard.tsx
@@ -3,9 +3,9 @@ import Countdown from "../../islands/updateTime.tsx";
 const collegeSvg = (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 16 16"
-    width="16"
-    height="16"
+    viewBox="0 0 30 30"
+    width="30"
+    height="30"
   >
     <path d="M1.75 16A1.75 1.75 0 0 1 0 14.25V1.75C0 .784.784 0 1.75 0h8.5C11.216 0 12 .784 12 1.75v12.5c0 .085-.006.168-.018.25h2.268a.25.25 0 0 0 .25-.25V8.285a.25.25 0 0 0-.111-.208l-1.055-.703a.749.749 0 1 1 .832-1.248l1.055.703c.487.325.779.871.779 1.456v5.965A1.75 1.75 0 0 1 14.25 16h-3.5a.766.766 0 0 1-.197-.026c-.099.017-.2.026-.303.026h-3a.75.75 0 0 1-.75-.75V14h-1v1.25a.75.75 0 0 1-.75.75Zm-.25-1.75c0 .138.112.25.25.25H4v-1.25a.75.75 0 0 1 .75-.75h2.5a.75.75 0 0 1 .75.75v1.25h2.25a.25.25 0 0 0 .25-.25V1.75a.25.25 0 0 0-.25-.25h-8.5a.25.25 0 0 0-.25.25ZM3.75 6h.5a.75.75 0 0 1 0 1.5h-.5a.75.75 0 0 1 0-1.5ZM3 3.75A.75.75 0 0 1 3.75 3h.5a.75.75 0 0 1 0 1.5h-.5A.75.75 0 0 1 3 3.75Zm4 3A.75.75 0 0 1 7.75 6h.5a.75.75 0 0 1 0 1.5h-.5A.75.75 0 0 1 7 6.75ZM7.75 3h.5a.75.75 0 0 1 0 1.5h-.5a.75.75 0 0 1 0-1.5ZM3 9.75A.75.75 0 0 1 3.75 9h.5a.75.75 0 0 1 0 1.5h-.5A.75.75 0 0 1 3 9.75ZM7.75 9h.5a.75.75 0 0 1 0 1.5h-.5a.75.75 0 0 1 0-1.5Z">
     </path>
@@ -15,9 +15,9 @@ const collegeSvg = (
 const locationSvg = (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 16 16"
-    width="16"
-    height="16"
+    viewBox="0 0 30 30"
+    width="30"
+    height="30"
   >
     <path d="m12.596 11.596-3.535 3.536a1.5 1.5 0 0 1-2.122 0l-3.535-3.536a6.5 6.5 0 1 1 9.192-9.193 6.5 6.5 0 0 1 0 9.193Zm-1.06-8.132v-.001a5 5 0 1 0-7.072 7.072L8 14.07l3.536-3.534a5 5 0 0 0 0-7.072ZM8 9a2 2 0 1 1-.001-3.999A2 2 0 0 1 8 9Z">
     </path>
@@ -27,9 +27,9 @@ const locationSvg = (
 const clockSvg = (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 16 16"
-    width="16"
-    height="16"
+    viewBox="0 0 30 30"
+    width="30"
+    height="30"
   >
     <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0ZM1.5 8a6.5 6.5 0 1 0 13 0 6.5 6.5 0 0 0-13 0Zm7-3.25v2.992l2.028.812a.75.75 0 0 1-.557 1.392l-2.5-1A.751.751 0 0 1 7 8.25v-3.5a.75.75 0 0 1 1.5 0Z">
     </path>
@@ -39,9 +39,9 @@ const clockSvg = (
 const languageSvg = (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 16 16"
-    width="16"
-    height="16"
+    viewBox="0 0 30 30"
+    width="30"
+    height="30"
   >
     <path d="M1.75 1h8.5c.966 0 1.75.784 1.75 1.75v5.5A1.75 1.75 0 0 1 10.25 10H7.061l-2.574 2.573A1.458 1.458 0 0 1 2 11.543V10h-.25A1.75 1.75 0 0 1 0 8.25v-5.5C0 1.784.784 1 1.75 1ZM1.5 2.75v5.5c0 .138.112.25.25.25h1a.75.75 0 0 1 .75.75v2.19l2.72-2.72a.749.749 0 0 1 .53-.22h3.5a.25.25 0 0 0 .25-.25v-5.5a.25.25 0 0 0-.25-.25h-8.5a.25.25 0 0 0-.25.25Zm13 2a.25.25 0 0 0-.25-.25h-.5a.75.75 0 0 1 0-1.5h.5c.966 0 1.75.784 1.75 1.75v5.5A1.75 1.75 0 0 1 14.25 12H14v1.543a1.458 1.458 0 0 1-2.487 1.03L9.22 12.28a.749.749 0 0 1 .326-1.275.749.749 0 0 1 .734.215l2.22 2.22v-2.19a.75.75 0 0 1 .75-.75h1a.25.25 0 0 0 .25-.25Z">
     </path>
@@ -51,34 +51,34 @@ const languageSvg = (
 export default function ProfileCard() {
   return (
     <div
-      className={"flex-auto profile-card shrink max-w-fit"}
+      className={"flex flex-col profile-card shrink max-w-fit"}
     >
-      <div style={{ width: "30%"}}>
+      <div style={{ width: "75%" }}>
         <img
           src="/25267581.png"
           alt="image of female knight holding a sword"
           style={{ border: "6px solid #555", borderRadius: "29.97%" }}
         />
       </div>
-      <div className={"max-w-fit"}>
+      <div class="flex flex-col pb-2">
         <a className="">Michael Gummere</a>
         <br />
         <a className="">@chrono__ / @chrono-byte</a>
       </div>
-      <ul class="max-w-lg">
-        <div>
+      <ul class="max-w-lg text-sm">
+        <div class="flex flex-auto pb-2">
           {collegeSvg}
           <a className="gold">Purdue University in Indianapolis</a>
         </div>
-        <div>
+        <div class="flex flex-auto pb-2">
           {locationSvg}
           Indianapolis, IN, USA
         </div>
-        <div>
+        <div class="flex flex-auto pb-2">
           {clockSvg}
           <Countdown />
         </div>
-        <div>
+        <div class="flex flex-auto pb-2">
           {languageSvg}
           🇺🇸 <a className="white" style={{ fontSize: "smaller" }}>(native)</a>
           {" "}
