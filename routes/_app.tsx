f5cb3db77697481f8aae87824d5dfba9aea2ab63
Michael Gummere <me@disilla.org>
2024-02-05 17:00:54 -0500

    set page title to name

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -6,7 +6,7 @@ export default function App({ Component }: PageProps) {
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-        <title>chrono-byte</title>
+        <title>Michael Gummere</title>
 
         <link rel="stylesheet" href="/styles.css" />
         <meta name="description" content="Hi I'm Michael." />
