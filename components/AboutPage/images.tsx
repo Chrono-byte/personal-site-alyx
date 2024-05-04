e07fce2a573f275f702f7d8d01c61ee7057f6fb2
Chrono <me@disilla.org>
2024-05-04 19:36:57 -0400

    switch to remote serve icons

--- components/AboutPage/images.tsx
+++ components/AboutPage/images.tsx
@@ -1,36 +1,27 @@
-export const collegeSvg = (
-  <img
-    src="/svg/college.svg"
-    alt="college icon"
-    title={"College"}
-    style={{
-      width: "30px",
-      height: "30px",
-    }}
-    className="pr-3"
-  />
-);
-export const locationSvg = (
-  <img
-    src="/svg/location.svg"
-    alt="location icon"
-    title={"Location"}
-    style={{
-      width: "30px",
-      height: "30px",
-    }}
-    className="pr-3"
-  />
-);
-export const clockSvg = (
-  <img
-    src="/svg/clock.svg"
-    alt="clock icon"
-    title={"Local Time to Me"}
-    style={{
-      width: "30px",
-      height: "30px",
-    }}
-    className="pr-3"
-  />
-);
+// export const collegeSvg = (
+//   <img
+//     src="/svg/college.svg"
+//     alt="college icon"
+//     title={"College"}
+//   />
+// );
+// export const locationSvg = (
+//   <img
+//     src="/svg/location.svg"
+//     alt="location icon"
+//     title={"Location"}
+//   />
+// );
+// export const clockSvg = (
+//   <img
+//     src="/svg/clock.svg"
+//     alt="clock icon"
+//     title={"Local Time to Me"}
+//   />
+// );
+
+import ClockIcon from "https://deno.land/x/tabler_icons_tsx@0.0.6/tsx/clock.tsx";
+import College from "https://deno.land/x/tabler_icons_tsx@0.0.6/tsx/building.tsx";
+import MapPin from "https://deno.land/x/tabler_icons_tsx@0.0.6/tsx/map-pin.tsx";
+
+export { ClockIcon, College, MapPin };
