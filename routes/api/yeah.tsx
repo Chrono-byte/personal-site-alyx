99d951001a630ac7745b27fbe1b36aa817d42de0
Michael Gummere <me@disilla.org>
2024-09-10 10:00:33 -0400

    remove yeah api

--- routes/api/yeah.tsx
+++
@@ -1,9 +0,0 @@
-import { PageProps } from "$fresh/server.ts";
-
-export default function Greet(props: PageProps) {
-  return (
-    <div>
-      <img id="image" src="../yeah.png" />
-    </div>
-  );
-}
